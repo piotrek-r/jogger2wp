1d9b19f2ac5b69b5ed8557d8f9acacbb4ef8b242
Piotrek Rybałtowski <piotrek@rybaltowski.pl>
2012-10-24 08:52:53 +0200

    First version of Application class

---
+++ classes/Jogger2Wp/Application.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Jogger2Wp;
+
+/**
+ * Application.php
+ *
+ * @author Piotr Rybałtowski <piotrek@rybaltowski.pl>
+ */
+class Application
+{
+    // region vars
+
+    /**
+     * @var array
+     */
+    protected $argv = null;
+
+    // endregion
+
+    // region constructor
+
+    /**
+     * @param array $argv
+     */
+    public function __construct($argv)
+    {
+        $this->argv = $argv;
+    }
+
+    // endregion
+
+    // region run
+
+    /**
+     * @return int
+     */
+    public function run()
+    {
+        if(!$this->checkArgv()) return $this->usage();
+
+        $sourceXmlPath = $this->argv[1];
+        $outputXmlPath = $this->argv[2];
+
+        if(!is_readable($sourceXmlPath)) {
+            printf("File %s is not readable\n", $sourceXmlPath);
+            return 10;
+        }
+
+        if(file_exists($outputXmlPath)) {
+            if(!is_writable($outputXmlPath)) {
+                printf("File %s is not writable\n", $outputXmlPath);
+                return 20;
+            }
+        }
+        else {
+            $outputXmlDir = dirname($outputXmlPath);
+            if(!is_writable($outputXmlDir)) {
+                printf("Directory of file %s is not writable\n", $outputXmlPath);
+                return 21;
+            }
+        }
+
+        $data = $this->parseXml($sourceXmlPath);
+
+        return 0;
+    }
+
+    // endregion
+
+    // region load/parse xml
+
+    /**
+     * @param string $sourceXmlPath
+     * @return array
+     */
+    public function parseXml($sourceXmlPath)
+    {
+        return array();
+    }
+
+    // endregion
+
+    // region console tools
+
+    /**
+     * @return boolean
+     */
+    public function checkArgv()
+    {
+        if(count($this->argv) !== 3) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return int
+     */
+    public function usage()
+    {
+        printf("Usage: %s <sourceXmlFile> <outputXmlFile>\n", $this->argv[0]);
+        return 1;
+    }
+
+    // endregion
+}
---
+++ jogger2wp.php
@@ -0,0 +1,16 @@
+<?php
+
+if('cli' !== PHP_SAPI) {
+    echo 'This is intended to be run as CLI command', PHP_EOL;
+    exit(1);
+}
+
+spl_autoload_register(function($name) {
+    $parts = explode('\\', $name);
+    array_unshift($parts, __DIR__, 'classes');
+    $path = implode(DIRECTORY_SEPARATOR, $parts) . '.php';
+    require_once $path;
+});
+
+$j2w = new Jogger2Wp\Application($argv);
+exit($j2w->run());
